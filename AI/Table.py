95693037272d90f2fa2dd5c671d7758d2208d813
Nick-Nack-1 <nicolaasm.steenkamp@gmail.com>
2025-01-09 10:42:11 +0200

    Refactor input handling and integrate mouse events for player interactions

--- AI/Table.py
+++ AI/Table.py
@@ -15,30 +15,47 @@ class Board():
             self.Board.append([])
             for x in range(Board_size[0]):
                 self.Board[y].append(0)
-        self.Board = [[-1,-1,-1],[0,0,0],[1,1,1]]
+        self.Board = [[3,3,3,3,3],
+                      [3,-1,-1,-1,3],
+                      [3,0,0,0,3],
+                      [3,1,1,1,3],
+                      [3,3,3,3,3]]
     
 
     def Update(self):
         Current_plr = self.Players[self.Turn]
-        move = Current_plr.Play(self.Board)
+        move = Current_plr.Play(self.Board, self.Turn)
         if move != None:
             if move[1] == "M":
                 self.Board[move[0][1]][move[0][0]] = 0
                 self.Board[move[0][1]-1][move[0][0]] = self.Turn
-            if move[1] == "R":
+            elif move[1] == "R":
                 self.Board[move[0][1]][move[0][0]] = 0
                 self.Board[move[0][1]-1][move[0][0]+1] = self.Turn
-            if move[1] == "L":
+            elif move[1] == "L":
                 self.Board[move[0][1]][move[0][0]] = 0
                 self.Board[move[0][1]-1][move[0][0]-1] = self.Turn
             self.Turn = self.Turn*-1
             self.Rotate()
     
 
+    def ValidateMove(self, move:list) -> bool:
+        if move[1] == "M":
+            if self.Board[move[0][1]-1][move[0][0]] == 0:
+                return True
+        elif move[1] == "R":
+            if self.Board[move[0][1]-1][move[0][0]+1] == self.Turn*-1:
+                return True
+        elif move[1] == "L":
+            if self.Board[move[0][1]-1][move[0][0]-1] == self.Turn*-1:
+                return True
+        return False
+    
+
     def Rotate(self):
         temp_board = copy.deepcopy(self.Board)
         for y in range(len(self.Board)):
             for x in range(len(self.Board[0])):
-                rev_x = abs(x-len(self.Board[0])-1)
-                rev_y = abs(y-len(self.Board)-1)
+                rev_x = abs(x-(len(self.Board[0])-1))
+                rev_y = abs(y-(len(self.Board)-1))
                 self.Board[y][x] = temp_board[rev_y][rev_x]
